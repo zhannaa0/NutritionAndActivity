7eb765533403c430e0047fc320f47edebd8b9a13
zhannaa0 <143710787+zhannaa0@users.noreply.github.com>
2024-02-11 22:17:40 +0600

    Update app.js

--- app.js
+++ app.js
@@ -1,67 +1,74 @@
 const express = require('express');
-const mongoose = require('mongoose')
-const app = new express();
+const mongoose = require('mongoose');
+const session = require('express-session');
+const app = express();
 const port = 3000;
-const WeatherRouter = require('./routes/routerWeather')
-const NasaRouter = require('./routes/routerNasa')
-const CatRouter = require('./routes/routerCats')
-const AnimalsRouter = require('./routes/routerAnimals')
-const bodyParser = require('body-parser')
-const User = require('./models/User')
-const userRouter = require('./routes/userRouter')
-const methodOverride = require('method-override')
-const pdfRouter = require('./routes/pdfRouter')
+const WeatherRouter = require('./routes/routerWeather');
+const NasaRouter = require('./routes/routerNasa');
+const CatRouter = require('./routes/routerCats');
+const AnimalsRouter = require('./routes/routerAnimals');
+const bodyParser = require('body-parser');
+const User = require('./models/User');
+const userRouter = require('./routes/userRouter');
+const methodOverride = require('method-override');
+const pdfRouter = require('./routes/pdfRouter');
 
+app.use(session({
+    secret: 'secret', 
+    resave: false,
+    saveUninitialized: true
+}));
 
 app.set('view engine', 'ejs');
 
-app.use(methodOverride('_method'))
+app.use(methodOverride('_method'));
 app.use(express.static('public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-app.use('/weather', WeatherRouter)
-app.use('/nasa', NasaRouter)
-app.use('/catgen', CatRouter)
-app.use('/animals', AnimalsRouter)
-app.use('/users', userRouter)
-app.use('/generatePdf', pdfRouter)
+const isAdmin = (req, res, next) => {
+    if (req.session.isAdmin) {
+        next();
+    } else {
+        res.status(403).send('You cannot access this page since you are not an admin.');
+    }
+};
 
-
+app.use('/weather', WeatherRouter);
+app.use('/nasa', NasaRouter);
+app.use('/catgen', CatRouter);
+app.use('/animals', AnimalsRouter);
+app.use('/users', userRouter);
+app.use('/generatePdf', pdfRouter);
 
 app.get('/', (req, res)=>{
     res.render('index');
-})
-
+});
 
 app.get('/nasa', (req, res)=>{
-    res.render('nasa')
-})
-
+    res.render('nasa');
+});
 
 app.get('/weather', (req, res)=>{
     res.render('weather');
-})
-
+});
 
 app.get('/catgen', (req, res)=>{
-    res.render('catgen')
-})
-
+    res.render('catgen');
+});
 
 app.get('/login', (req, res)=>{
-    res.render('login')
-})
+    res.render('login');
+});
 
 app.get('/signUp', (req, res)=>{
-    res.render('signUp')
-})
+    res.render('signUp');
+});
 
-app.get('/admin', async (req, res) => {
+app.get('/admin', isAdmin, async (req, res) => {
     try {
         const users = await User.find({}, 'name username isAdmin creationDate updateDate deletionDate');
-
         res.render('admin', { users });
     } catch (err) {
         console.error('Error fetching users:', err);
@@ -70,22 +77,19 @@ app.get('/admin', async (req, res) => {
 });
 
 app.get('/animals', (req, res)=>{
-    res.render('animals')
-})
+    res.render('animals');
+});
 
-app.get('/addUser', (req, res)=>{
-    res.render('addUser')
-})
-
-
+app.get('/addUser', isAdmin, (req, res)=>{
+    res.render('addUser');
+});
 
 app.listen(port, ()=>{
-    console.log(`App is listening on port ${port} ...`)
-})
+    console.log(`App is listening on port ${port} ...`);
+});
 
 mongoose.connect("mongodb+srv://zhanna14:zhanna123@cluster0.4ow5nxc.mongodb.net/zhanna?retryWrites=true&w=majority").then(async () => {
-
-  app.listen(5010, () => {
-    console.log("Connected to database and listening on port 5010");
-  });
+    app.listen(5010, () => {
+        console.log("Connected to database and listening on port 5010");
+    });
 }).catch((err) => console.error('Error connecting to database:', err));
